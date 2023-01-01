bacfd91ad4fdac679c9094b4a21058f6f5a565cb
omarzouaoui <zouaouiomar80@gmail.com>
2023-01-01 11:35:28 +0100

    List of Cart Items

--- grocery_shop_app/lib/pages/cart_page.dart
+++ grocery_shop_app/lib/pages/cart_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_shop_app/model/cart_model.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
@@ -10,6 +12,47 @@ class CartPage extends StatelessWidget {
         title: Text("My Cart Page"),
         centerTitle: true,
       ), 
+      body: Consumer<CartModel>(builder: (context, value, child) {
+        return Column(
+          children: [
+            //list of cart item
+            Expanded(
+              child: ListView.builder(
+                itemCount: value.cartItems.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade200,
+                        borderRadius: BorderRadius.circular(8)
+                      ),
+                      child: ListTile(
+                        leading: Image.asset(
+                          value.cartItems[index][2],
+                          height: 36,
+                          ),
+                        title: Text(
+                          value.cartItems[index][0],
+                        ),
+                        subtitle: Text(
+                          "\$ "+value.cartItems[index][1]
+                        ),
+                        trailing: IconButton(
+                        onPressed: () =>
+                        Provider.of<CartModel>(context, listen: false)
+                        .removeItemFromCart(index), 
+                        icon: Icon(Icons.cancel)
+                        )
+                      ),
+                    ),
+                  );
+                },
+                )
+              )
+          ],
+        );
+      },)
     );
   }
 }
