7859aad7fa21980d4504656c224858c7be67bf30
omarzouaoui <zouaouiomar80@gmail.com>
2023-01-01 08:30:32 +0100

    Google Fonts & intro page

--- grocery_shop_app/lib/pages/intro_page.dart
+++ grocery_shop_app/lib/pages/intro_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
@@ -10,9 +11,63 @@ class IntroPage extends StatelessWidget {
         children: [
           //logo
           Padding(
-            padding: const EdgeInsets.all(80.0),
+            padding: const EdgeInsets.only(
+              left: 80.0, right: 80.0, bottom: 40.0, top: 160.0
+            ),
             child: Image.asset("lib/images/avocado.png"),
-            )
+            ),
+
+            //we deliver
+              Padding(
+              padding: EdgeInsets.all(24.0),
+              child: Text(
+                "We deliver groceries at your doorstep",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.notoSerif(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 24,),
+
+            //fresh items everyday
+            Text(
+              "Fresh items everyday",
+              style: TextStyle(
+                color: Colors.grey.shade400,
+              ),
+            ),
+
+            const Spacer(),
+
+            //get started button
+            GestureDetector(
+              onDoubleTap: () => Navigator.pushReplacement(
+                context, MaterialPageRoute(
+                  builder: (context){
+                  return 
+                })
+              ) ,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                padding: const EdgeInsets.all(24.0),
+                child: const Text(
+                  "Get Started",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+
+            const Spacer(),
+
         ],
       ),
     );
