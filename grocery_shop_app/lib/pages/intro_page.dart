b9a62262371c8f2e9162ee34e76ee516241bffea
omarzouaoui <zouaouiomar80@gmail.com>
2023-01-01 08:03:06 +0100

    images & intro page

---
+++ grocery_shop_app/lib/pages/intro_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          //logo
+          Padding(
+            padding: const EdgeInsets.all(80.0),
+            child: Image.asset("lib/images/avocado.png"),
+            )
+        ],
+      ),
+    );
+  }
+}
